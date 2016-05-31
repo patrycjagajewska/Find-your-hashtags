8658a16afe1c7683fd8e83a63476434b5f3796b4
zuzannachrzastek <zuzanna.chrzastek@gmail.com>
2016-05-31 17:45:29 +0200

    service interface

--- service/src/main/java/pl/edu/agh/tai/service/HashtagService.java
+++ service/src/main/java/pl/edu/agh/tai/service/HashtagService.java
@@ -1,7 +1,13 @@
 package pl.edu.agh.tai.service;
 
+import twitter4j.Twitter;
+
 /**
  * Created by Zuzia on 2016-05-31.
  */
 public class HashtagService implements IHashtagService{
+    @Override
+    public void displayTimelineTweets(Twitter twitter) {
+
+    }
 }
--- service/src/main/java/pl/edu/agh/tai/service/IHashtagService.java
+++ service/src/main/java/pl/edu/agh/tai/service/IHashtagService.java
@@ -4,4 +4,7 @@ package pl.edu.agh.tai.service;
  * Created by Zuzia on 2016-05-31.
  */
 public interface IHashtagService {
+    void displayTimelineTweets(twitter4j.Twitter twitter);
+    void searchForHashtag(twitter4j.Twitter twitter, String hashtag, Integer howMany);
+    void markAsFavourite(twitter4j.Twitter twitter, Long tweetID);
 }
