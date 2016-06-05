2dff9d4b9b40cc5af5a07935110812c582579ebb
Patrycja Wrona <patrycjawrona23@gmail.com>
2016-06-05 18:04:46 +0200

    Few angular services implemented

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -3,3 +3,35 @@
 angular.module('findyourhashtags.controllers', [])
 
 
+    .controller('LoginController', function ($scope, $rootScope, $state, $location, Restangular) {
+
+        $scope.login = function(){
+            Restangular.all('login').post('username=' + $scope.credentials.username
+                + '&password=' + $scope.credentials.password).then(function(resp){
+                console.log(resp);
+                $location.path("/gifts");
+
+            }, function(resp){
+                console.log(resp);
+            });
+        };
+
+        $scope.logout = function(){
+            Restangular.all('logout').post().then(function(resp){
+                console.log(resp);
+            }, function(resp){
+                console.log(resp);
+            });
+            $location.path('login');
+
+        };
+    })
+
+    .controller('HashtagController', function ($scope, $state, HashtagService) {
+        var tweets = HashtagService.getTweets();
+
+        $scope.tweets = tweets;
+    });
+
+
+
--- web/js/services.js
+++ web/js/services.js
@@ -1,3 +1,56 @@
 'use strict';
 
 angular.module('findyourhashtags.services', [])
+    .factory('HashtagService', function ($resource, serverUrl) {
+        var service = {};
+
+        service.getTweets = function() {
+            Restangular.all('tweets').getList().then(function(resp){
+                console.log(resp);
+            });
+        };
+
+        service.getTweet = function(tweetId){
+            Restangular.all('tweets').one(tweetId).get().then(function(resp){
+                console.log(resp);
+            });
+        };
+
+        service.searchHashtag = function(hashtag) {
+            Restangular.all('tweets').one(hashtag).get().then(function(resp){
+                console.log(resp);
+            });
+        };
+
+        service.retweet = function(){
+          Restangular.one(tweetId).get().then(function(data){
+
+            });
+        };
+
+        //service.getTweets() = function() {
+        //    return $resource(serverUrl + "/tweets").query();
+        //};
+        //
+        //service.searchHashtag = function(hashtag) {
+        //    return $resource(serverUrl + "/tweets/" + hashtag).get();
+        //};
+        //
+        //service.retweet = function(tweetId) {
+        //    return $resource(serverUrl + "/tweets/retweet/" + tweetId).get();
+        //};
+        //
+        //service.favourite = function(tweetId) {
+        //    return $resource(serverUrl + "/tweets/favourite/" + tweetId).get();
+        //};
+        //
+        //service.getComments = function(tweetId) {
+        //    return $resource(serverUrl + "/tweets/comments/" + tweetId).get();
+        //};
+        //
+        //service.comment = function(tweetId) {
+        //    return $resource(serverUrl + "/tweets/comment/" + tweetId).get();
+        //};
+
+        return service;
+    });
