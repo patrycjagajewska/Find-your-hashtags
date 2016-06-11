72fe3fadfb54c80f2ab5d7ed82beec38f94ecdc0
zuzannachrzastek <zuzanna.chrzastek@gmail.com>
2016-06-11 10:31:39 +0200

    Procfile and spring properties added

--- app/src/main/java/pl/edu/agh/tai/app/Application.java
+++ app/src/main/java/pl/edu/agh/tai/app/Application.java
@@ -2,6 +2,7 @@ package pl.edu.agh.tai.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import twitter4j.*;
 
 import java.util.List;
