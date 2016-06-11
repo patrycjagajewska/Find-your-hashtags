db52f28f9fdf84880e445e159ce0825c384e1f03
zuzannachrzastek <zuzanna.chrzastek@gmail.com>
2016-06-11 10:38:50 +0200

    app config

---
+++ app/src/main/java/pl/edu/agh/tai/app/AppConfig.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.tai.app;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * Created by Zuzia on 2016-06-11.
+ */
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class AppConfig {
+}
