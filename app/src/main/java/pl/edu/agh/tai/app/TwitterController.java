e1433d8dac940ce828b6aa6546f8d7c52af6484a
Patrycja Wrona <patrycjawrona23@gmail.com>
2016-06-26 11:08:58 +0200

    Security removed from branch master

--- app/src/main/java/pl/edu/agh/tai/app/TwitterController.java
+++ app/src/main/java/pl/edu/agh/tai/app/TwitterController.java
@@ -15,13 +15,8 @@ import java.util.Map;
 @RequestMapping("/tweets")
 public class TwitterController {
 
-    @Autowired
-    private OAuthToken authToken;
-
     private TwitterFactory factory = new TwitterFactory();
     private Twitter twitter = factory.getInstance();
-
-    private MyAccessToken myAccessToken;
 
     List<TweetStatus> tweetStatuses = new ArrayList<>();
     List<TweetStatus> tweetStatusesHashtag = new ArrayList<>();
