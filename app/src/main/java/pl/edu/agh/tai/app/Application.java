99e78ec51968625d46aa1db42965dc08f6a5fd5f
Patrycja Wrona <patrycjawrona23@gmail.com>
2016-06-30 00:04:42 +0200

    Spring configuration file added

--- app/src/main/java/pl/edu/agh/tai/app/Application.java
+++ app/src/main/java/pl/edu/agh/tai/app/Application.java
@@ -2,8 +2,8 @@ package pl.edu.agh.tai.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
-import twitter4j.*;
+import org.springframework.context.annotation.ImportResource;
+import twitter4j.Status;
 
 import java.util.List;
 import java.util.Map;
@@ -12,51 +12,11 @@ import java.util.TreeMap;
 /**
  * Created by Zuzia on 2016-06-01.
  */
-
+@ImportResource("classpath:config.xml")
 @SpringBootApplication
 public class Application {
-    private static Map<Long, Status> tweetsMap = new TreeMap<Long, Status>();
-    private static List<Status> tweets;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    public static void searchForHashtag(twitter4j.Twitter twitter, String hashtag, Integer howMany){
-        try {
-            if(!hashtag.startsWith("#")){
-                hashtag = "#" + hashtag;
-                System.out.println(hashtag);
-            }
-
-            QueryResult result;
-            Query query = new Query(hashtag);
-            int i = 0;
-            do{
-                result = twitter.search(query);
-                tweets = result.getTweets();
-
-                for(Status tweet : tweets){
-                    tweetsMap.put(tweet.getId(), tweet);
-                }
-
-                for(Map.Entry entry : tweetsMap.entrySet()) {
-                    i++;
-                    Status tweet = (Status) entry.getValue();
-                    Long tweetID = (Long) entry.getKey();
-                    System.out.println(i);
-                    System.out.println(tweetID);
-//                    System.out.println("@" + tweet.getUser().getName() + " ----- " + tweet.getText());
-
-                    if (howMany != null) {
-                        if(i == howMany) break;
-                    }
-                }
-                System.out.println("---------------end of while-------------");
-            } while(((query = result.nextQuery()) != null) && i != howMany);
-            System.exit(0);
-        } catch (TwitterException e) {
-            e.printStackTrace();
-        }
     }
 }
